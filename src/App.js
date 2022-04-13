3d35fb286e63d774eee3f557b22d4a17162acbbd
avarus-dei <klimentiberulava55@gmail.com>
2022-04-13 20:23:48 +0400

    essentian files added
    basic functionality added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import "./App.css";
+import { Fragment } from "react";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
-  return <div></div>;
+  return (
+    <Fragment>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
+  );
 }
 
 export default App;
